3cd4aef281575a8f9f980450295690af4f766b2c
Nguyễn Việt Kim <114214103+KGC31@users.noreply.github.com>
2022-10-13 09:23:21 +0700

    Add files via upload

---
+++ Windows functions.cpp
@@ -0,0 +1,78 @@
+#include<windows.h>
+#include<conio.h>
+
+using namespace std;
+
+//This function is used to set text color
+void Set_Color(int color) 
+{
+    WORD wColor;
+
+    HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    if (GetConsoleScreenBufferInfo(hStdOut, &csbi))
+    {
+        wColor = (csbi.wAttributes & 0xF0) + (color & 0x0F);
+        SetConsoleTextAttribute(hStdOut, wColor);
+    }
+}
+
+//This function is used to set psotion of cursor
+void Gotoxy(short x, short y) 
+{
+    HANDLE hConsoleOutput;
+    COORD Cursor_an_Pos = { x, y };
+    hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleCursorPosition(hConsoleOutput, Cursor_an_Pos);
+}
+
+//This function is used to Show cursor or not
+void ShowCur(bool CursorVisibility)
+{
+    HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_CURSOR_INFO ConCurInf;
+
+    ConCurInf.dwSize = 10;
+    ConCurInf.bVisible = CursorVisibility;
+
+    SetConsoleCursorInfo(handle, &ConCurInf);
+}
+
+//This function is used to disable resize console window
+void DisableResizeWindow()
+{
+    HWND hWnd = GetConsoleWindow();
+    SetWindowLong(hWnd, GWL_STYLE, GetWindowLong(hWnd, GWL_STYLE) & ~WS_SIZEBOX);
+}
+
+//This function is used to change key buffer size
+void SetScreenBufferSize(SHORT width, SHORT height)
+{
+    HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    COORD NewSize;
+    NewSize.X = width;
+    NewSize.Y = height;
+
+    SetConsoleScreenBufferSize(hStdout, NewSize);
+}
+
+//This function is used to disable selecting text
+void DisableSelection()
+{
+    HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
+
+    SetConsoleMode(hStdin, ~ENABLE_QUICK_EDIT_MODE);
+}
+
+void Set_Window_Size(SHORT width, SHORT height) {
+    HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    SMALL_RECT WindowSize;
+    WindowSize.Top = 0;
+    WindowSize.Left = 0;
+    WindowSize.Right = width;
+    WindowSize.Bottom = height;
+
+    SetConsoleWindowInfo(hStdout, 1, &WindowSize);
+}
